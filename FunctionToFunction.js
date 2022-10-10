cb0f959e52f276a797fbd45b6f1a080bb7ff2df6
Alesandro Dragnev <92319051+alesandro2251@users.noreply.github.com>
2022-10-10 21:58:42 +0300

    Delete FunctionToFunction.js

--- FunctionToFunction.js
+++
@@ -1,8 +0,0 @@
-
-// Funtcions calling Functions
-
-function fruits(apples, oranges){
-    console.log(apples, oranges);
-    const juice = `The juice is made out ${apples} apple and ${oranges} oranges .`;
-    return juice; // returns the value of the variable juice
-}
